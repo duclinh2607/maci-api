01ea4cc1f229f343137b0d831e62d33e83ef84c2
duclinh2607 <huynhductk6@gmail.com>
2026-05-22 22:48:05 +0700

    Delete check.php

--- check.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: application/json");
-
-$allowed = ["12345","VIP001","ADMIN999"];
-$code = $_GET["code"] ?? "";
-
-echo json_encode(["allowed" => in_array($code, $allowed)]);
-?>
